fbf4fe318656c4559e7a4c746204a6ea3eb55ee7
Kent Rasmussen <kent_rasmussen@sil.org>
2025-07-09 16:15:56 -0500

    new forms as wrappers with options

--- train_bert.py
+++ train_bert.py
@@ -1,122 +1,79 @@
 #!/usr/bin/env python3
 # coding=UTF-8
-# This was originally based on https://huggingface.co/blog/fine-tune-w2v2-bert,
-# though it has been generalized and adapted from other sources, as well.
 
-# from transformers import TrainingArguments, Trainer, AutoModelForCTC
-# from transformers import Wav2Vec2CTCTokenizer, SeamlessM4TFeatureExtractor
-# from transformers import Wav2Vec2BertForCTC, Wav2Vec2BertProcessor
-from transformers import pipeline
-from transformers import BitsAndBytesConfig
-from dataclasses import dataclass
-import evaluate
-import torch
-import torchaudio
-import numpy
-from typing import Any, Dict, List, Union
-import sys,os
-import datetime
-import json
-import train #local
-
-def marktime(f,*args,**kwargs):
-    def timed(*args,**kwargs):
-        start_time=datetime.datetime.now(datetime.UTC)
-        r=f(*args,**kwargs)
-        print("Function",f.__class__.__name__,datetime.datetime.now(datetime.UTC)-start_time)
-        return r
-    return timed
-class Data(train.Data):
-    def make_vocab_file(self):
-        if not self.load_chars_from_file():
-            self.do_extract_all_chars()
-        #use defaults from https://huggingface.co/transformers/v4.5.1/model_doc/wav2vec2.html#wav2vec2ctctokenizer
-        if True: #I need to think if this is a good idea:
-            self.vocab_dict["|"] = self.vocab_dict[" "]#"make 'sp' more visible"
-            del self.vocab_dict[" "]
-        self.vocab_dict["<unk>"] = len(self.vocab_dict)
-        self.vocab_dict["<pad>"] = len(self.vocab_dict)
-        len(self.vocab_dict)
-        with open(f"vocab_{self.language['iso']}.json", 'w') as vocab_file:
-            json.dump(self.vocab_dict, vocab_file)
-    def __init__(self,**kwargs):
-        kwargs['make_vocab']=True
-        super().__init__(**kwargs)
-class TrainWrapper(train.TrainWrapper):
-    def __init__(self,model_type,trainer_type,my_options_args):
-        self.get_names(model_type,trainer_type,my_options_args)
-        self.init_debug()
-        self.processor_fn_kwargs=self.names.processorkwargs()
-        self.get_data_processor_model()
-        self.get_trainer()
-        super().__init__()
-        #in compute_metrics only:
-        self.metric = evaluate.load(self.names.metric_name)
-        self.do_stuff()
-def make_options():
-    import options
-    return options.Parser('train','infer')
 if __name__ == '__main__':
+    import sys,os
+    import train #local
     if not train.in_venv():
         print("this script is mean to run in a virtual environment, but isn't.")
-    my_options=make_options()
+    my_options=train.options.Parser('train','infer')
+    # my_options=make_options()
     """‘refresh_data’ happens anyway if processor is remade"""
     """‘remake_processor’ happens anyway if not found"""
     """‘reload_model’ causes a large download!"""
+    from model_configs import mms
+    # model_type=w2v_bert_2()
+    model_type=mms()
+    model_type.update({'fqbasemodelname':"facebook/mms-1b-all"})
+    print(f"working with model {model_type['fqbasemodelname']}")
+    """This is general settings"""
     my_options.args.update({
-            'language_iso':'gnd',
-            'cache_dir':'/media/kentr/hfcache',
-            'dataset_code':'csv',
-            'data_file_prefixes':['lexicon_640'],#,'examples_4589'
-            'data_file_location':'training_data',
-            'infer_checkpoints':True,
-            'train':True,
-            'infer':True,
-            # 'refresh_data':True,
-            # 'remake_processor':True, #
-            # 'reload_model':True
-        })
-    if 'google.colab' in sys.modules:
-        print("It looks like we're running in a colab instance, so setting "
-            "some variables now.")
-        my_options.args.update({
-                'cache_dir':'.',
-            })
-    my_options.sanitize() # wait until everyting is set to do this
-    from model_configs import w2v_bert_2
-    model_type=w2v_bert_2()
-    # {
-    #             'fqbasemodelname':"facebook/w2v-bert-2.0",
-    #             'getmodel_fn':Wav2Vec2BertForCTC, #for tuned models
-    #             'tokenizer_fn':Wav2Vec2CTCTokenizer,
-    #             'feature_extractor_fn':SeamlessM4TFeatureExtractor,
-    #             # 'processor_fn':Wav2Vec2BertProcessor,
-    #             'processor_fn':Processor,
-    #             }
+                            'cache_dir':'/media/kentr/hfcache',
+                            'data_file_location':'training_data',
+                            'train':True,
+                            # 'infer':True,
+                            # 'infer_checkpoints':True,
+                            'refresh_data':True, #any case w/new processor
+                            # 'remake_processor':True, #any case if not found
+                            # 'reload_model':True #large download!
+                            })
+    """Pick one of the following three:"""
+    # my_options.args.update(train.options.minimal_zulgo_test_options())
+    # my_options.args.update({
+    #                         'dataset_code':'mcv17',
+    #                         'data_splits':['train','validation'],
+    #                         'language_iso':'hau',
+    #                         'sister_language_iso': 'hau',
+    #                         # 'max_data_rows':20,
+    #                         })
+    my_options.args.update({
+                            'dataset_code':'csv',
+                            'language_iso':'gnd',
+                            'data_file_prefixes':['lexicon_640'],
+                            # 'data_file_prefixes':['examples_4589'],
+                            # 'data_file_prefixes':['lexicon_13'],
+                            })
+    print(my_options.args)
     trainer_type={
-                # 'data_collator_fn':DataCollatorCTCWithPadding,
-                # 'training_args_fn':TrainingArguments,
-                # 'trainer_fn':Trainer,
-                'learning_rate':1e-5,
-                'per_device_train_batch_size':16,
-                'save_steps':20,
+                'gradient_checkpointing':True,
+                # 'learning_rate':1e-5,
+                'learning_rate':1e-3,
+                # 'per_device_train_batch_size':16,
+                # 'per_device_train_batch_size':32,
+                'per_device_train_batch_size':64,
+                # 'save_steps':20,
                 # load_best_model_at_end requires the save and eval strategy to match
                 'eval_strategy': 'epoch',
                 'save_strategy': 'epoch',
                 # 'eval_steps':5,
-                'logging_steps':20,
-                'save_total_limit':6,
-                'num_train_epochs':1,
-                'attention-dropout':0.0,
-                'hidden-dropout':0.0,
-                'feat-proj-dropout':0.0,
-                'mask-time-prob':0.0,
-                'layerdrop':0.0,
-                'ctc-loss-reduction':'mean'
-                # 'compute_metrics' is hardcoded; add flexibility if needed
-                }
+                # 'logging_steps':20,
+                'save_total_limit':3,
+                'num_train_epochs':12,
+                # 'attention-dropout':0.0,
+                # 'hidden-dropout':0.0,
+                # 'feat-proj-dropout':0.0,
+                # 'mask-time-prob':0.0,
+                # 'layerdrop':0.0,
+                # 'ctc-loss-reduction':'mean'
+                'resume_from_checkpoint':True,
+                # above if last checkpoint is saved correctly, below if not
+                # 'resume_from_checkpoint':os.path.join(
+                #                 my_options.args['cache_dir'],
+                #                 "mms-1b-all-cer-gnd-Zulgo_640x6/checkpoint-18")
+            }
     for my_options.args['metric_name'] in ['cer','wer']:
-        TrainWrapper(model_type,trainer_type,my_options.args)
+        train.TrainWrapper(model_type,trainer_type,my_options)
+        del trainer_type['resume_from_checkpoint']
     exit()
     for my_options.args['data_file_prefixes'] in [
                                         ['lexicon_640','examples_300'],
--- train_whisper.py
+++ train_whisper.py
@@ -1,146 +1,68 @@
 #!/usr/bin/env python3
 # coding=UTF-8
-#!/usr/bin/env python3.12
-#3.11 until we get bz2 working in 3.12
-
-#pip install --upgrade pip
-#pip install --upgrade datasets[audio] transformers accelerate evaluate jiwer tensorboard gradio
-
-#https://huggingface.co/blog/fine-tune-whisper
-# from huggingface_hub import notebook_login
-#
-# notebook_login()
-# import bz2
-# exit()
-# from datasets import load_dataset, DatasetDict, concatenate_datasets, Audio
-# from transformers import WhisperFeatureExtractor, WhisperTokenizer
-# from transformers import WhisperProcessor
-# from transformers import WhisperForConditionalGeneration
-# from transformers import Seq2SeqTrainingArguments, Seq2SeqTrainer
-#for DataCollatorSpeechSeq2SeqWithPadding:
-# from dataclasses import dataclass
-import evaluate
-import torch
-# from typing import Any, Dict, List, Union
-import sys
-import train #this is mine
-
-class TrainWrapper(train.TrainWrapper):
-    # def get_base_model(self):
-    #     model=train.BaseModel(
-    #                     vocab_size=len(self.processor.tokenizer),
-    #                     pad_token_id=self.processor.tokenizer.pad_token_id,
-    #                     **self.names.modelkwargs()
-    #                     )
-    #     #BaseModel object goes away at this point
-    #     self.model=model.model
-        # return {"wer": wer}
-    # def get_processor(self):
-    #     processor=self.names.processor_fn(**self.names.processorkwargs())
-    #     #any other processing goes here:
-    #     processor.do_prepare_dataset(self.data)
-    #     self.processor=processor.processor
-    def set_whisper_langs(self):
-        # print("possible language codes: "
-        # f"{[i.strip('<>|') for i in self.model.generation_config.lang_to_id.keys()]}")
-        self.model.generation_config.language = self.names.sister_language['mcv_code']
-        self.model.generation_config.task = "transcribe"
-        self.model.generation_config.forced_decoder_ids = None
-        # self.model.generation_config.use_cache=True
-        self.model.generation_config.use_cache=self.names.use_cache_in_training
-    def __init__(self,model_type,trainer_type,my_options_args):
-        # past_key_values=EncoderDecoderCache.from_legacy_cache(past_key_values)
-        self.get_names(model_type,trainer_type,my_options_args)
-        self.init_debug()
-        self.processor_fn_kwargs=self.names.processorkwargs()
-        self.get_data_processor_model()
-        self.set_whisper_langs()
-        self.collator_fn_kwargs={'processor':self.processor,
-            'decoder_start_token_id':self.model.config.decoder_start_token_id}
-        self.get_trainer()
-        super().__init__()
-        #in compute_metrics only:
-        self.metric = evaluate.load(self.names.metric_name)
-        self.do_stuff()
-# def clear_unused_args(x):
-#     for arg in ['attention_dropout',
-#                 'hidden_dropout',
-#                 'feat_proj_dropout',
-#                 'mask_time_prob',
-#                 'layerdrop',
-#                 'ctc_loss_reduction',
-#                 ]:
-#         x.pop(arg)
-def make_options():
-    import options
-    return options.Parser('train','infer')
 if __name__ == '__main__':
+    import sys
+    import train #this is mine
     if not train.in_venv():
         print("this script is mean to run in a virtual environment, but isn't.")
-    my_options=make_options()
-    """‘refresh_data’ happens anyway if processor is remade"""
-    """‘remake_processor’ happens anyway if not found"""
-    """‘reload_model’ causes a large download!"""
-    my_options.args.update({
-            'dataset_code':'mcv17',
-            'data_splits':['train','validation'],
-            'language_iso':'hau',
-            # 'dataset_code':'csv',
-            # 'language_iso':'gnd',
-            # 'sister_language_iso': 'hau',
-            'sister_language_iso': 'hau',
-            'cache_dir':'/media/kentr/hfcache',
-            # 'data_file_prefixes':['lexicon_640'],#,'examples_4589'
-            # 'data_file_prefixes':['lexicon_13'],#,'examples_4589'
-            # 'data_file_location':'training_data',
-            'infer_checkpoints':True,
-            'train':True,
-            'infer':True,
-            # 'refresh_data':True,
-            # 'remake_processor':True, #
-            # 'reload_model':True
-        })
-    if 'google.colab' in sys.modules:
-        print("It looks like we're running in a colab instance, so setting "
-            "some variables now.")
-        my_options.args.update({
-                'cache_dir':'.',
-            })
-    my_options.sanitize() # wait until everyting is set to do this
     from model_configs import whisper
     model_type=whisper()
-    model_type.update({'fqbasemodelname':"openai/whisper-tiny"})
-    # model_type={
-    #             # 'fqbasemodelname':"openai/whisper-tiny", #small,large, medium, base, large-v3-turbo
-    #             'fqbasemodelname':'openai/whisper-large-v3',
-    #             # "efficient-speech/lite-whisper-large-v3-turbo"
-    #             'getmodel_fn':WhisperForConditionalGeneration, #for tuned models
-    #             'tokenizer_fn':WhisperTokenizer,
-    #             'feature_extractor_fn':WhisperFeatureExtractor,
-    #             'processor_fn':Processor,
-    #             }
+    model_type.update({'fqbasemodelname':
+                    "kent-rasmussen/whisper-large-v3-cer-hau-Hausa_mcv11x9"})
+    """ Also: openai/whisper-tiny, openai/whisper-small, openai/whisper-large,
+    openai/whisper-large-v2, openai/whisper-medium, openai/whisper-large-v3 """
 
+    my_options=train.options.Parser('train','infer')
+    my_options.args.update({ """This is general settings"""
+                            'cache_dir':'/media/kentr/hfcache',
+                            'data_file_location':'training_data',
+                            'train':True,
+                            # 'train_adaptor_only':True, #dunno how whisper
+                            # 'infer':True,
+                            # 'infer_checkpoints':True,
+                            # 'refresh_data':True, #any case w/new processor
+                            # 'remake_processor':True, #any case if not found
+                            # 'reload_model':True #large download!
+                            })
+    """Pick one of the following three:"""
+    # my_options.args.update(train.options.minimal_zulgo_test_options())
+    # my_options.args.update({
+    #                         'dataset_code':'mcv17',
+    #                         'data_splits':['train','validation'],
+    #                         'language_iso':'hau',
+    #                         'sister_language_iso': 'hau',
+    #                         # 'max_data_rows':20,
+    #                         })
+    my_options.args.update({
+                            'dataset_code':'csv',
+                            'language_iso':'gnd',
+                            'data_file_prefixes':['lexicon_640'],
+                            # 'data_file_prefixes':['examples_4589'],
+                            # 'data_file_prefixes':['lexicon_13'],
+                            })
     trainer_type={
-                # 'data_collator_fn':DataCollatorSpeechSeq2SeqWithPadding,
-                # 'training_args_fn':Seq2SeqTrainingArguments,
-                # 'trainer_fn':Seq2SeqTrainer,
-                'learning_rate':1e-5,
+                'learning_rate':1e-3,
                 # 'lr_scheduler_type':"linear",
                 'per_device_train_batch_size':16,
-                'save_steps':20,
+                # 'save_steps':20,
                 # load_best_model_at_end requires the save and eval strategy to match
-                # 'eval_strategy': 'epoch',
-                # 'save_strategy': 'epoch',
-                'eval_strategy': 'steps',
-                'save_strategy': 'steps',
-                'eval_steps':5,
+                'eval_strategy': 'epoch',
+                'save_strategy': 'epoch',
+                # 'eval_strategy': 'steps',
+                # 'save_strategy': 'steps',
+                'eval_steps':20,
                 'logging_steps':20,
-                'save_total_limit':6,
-                'num_train_epochs':9,
-                # 'compute_metrics' is hardcoded; add flexibility if needed
+                'save_total_limit':3,
+                'num_train_epochs':6,
+                'resume_from_checkpoint':True,
+                # above if last checkpoint is saved correctly, below if not
+                # 'resume_from_checkpoint':os.path.join(
+                #                 my_options.args['cache_dir'],
+                #                 "mms-1b-all-cer-gnd-Zulgo_640x6/checkpoint-18")
                 }
     for my_options.args['metric_name'] in ['wer','cer']:
-        TrainWrapper(model_type,trainer_type,my_options.args)
+        train.TrainWrapper(model_type,trainer_type,my_options)
+        del trainer_type['resume_from_checkpoint']
     exit()
     for my_options.args['data_file_prefixes'] in [
                                         ['lexicon_640','examples_300'],
